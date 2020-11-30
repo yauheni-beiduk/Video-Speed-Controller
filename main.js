32b63263555c7382747e5c20ebf1a1ddea0b2bd5
Yauheni Beiduk <yauheni.beiduk@gmail.com>
2020-11-30 22:45:27 +0300

    fix:add files

---
+++ main.js
@@ -0,0 +1,5 @@
+const video = document.querySelector('.video'),
+    speed = document.querySelector('.speed');
+    speedBar = document.querySelector('.speed-bar');
+
+speed.addEventListener('mousemove',)
